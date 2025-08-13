c9f6e48e6d0ba1191f0b8929942aacdc6f3bd28f
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2025-08-13 15:37:46 -0300

    mudanças

--- js/encomendas.js
+++ js/encomendas.js
@@ -1,6 +1,15 @@
-let qtd = document.querySelectorAll(".qtd")
-let unidade = document.querySelectorAll(".unitario")
-let preço = parseFloat(unidade.replace(',' , '.'));
-let resultado = preço * qtd
-let round = resultado.toFixed(2).replace('.' , ',')
-let total = document.querySelector('.total').innerHTML = round
+let encomenda_cliente = document.querySelectorAll(".encomenda-cliente")
+for(i=0; i < encomenda_cliente.length; i++) {
+     let clientes = encomenda_cliente[i];
+     console.log(clientes)
+     // pega cada elemento filho do td
+     let qtd = clientes.children[2]. textContent 
+     let unitario = clientes.children[3].textContent 
+     console.log(qtd)
+     console.log(unitario)
+     let num_qtd = parseInt(qtd)
+     let num_unitario = parseFloat(unitario.replace(',', '.'))
+     let resultado = num_qtd * num_unitario; // armazena cada resultado
+     console.log(resultado)
+     clientes.children[4].textContent = resultado.toFixed(2)
+}
