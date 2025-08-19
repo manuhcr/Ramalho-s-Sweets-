a5d24237c03bf8ac56039ba817d6c2a0a9c269aa
Manoela Cruz <168127407+manuhcr@users.noreply.github.com>
2025-08-18 22:16:40 -0300

    AAA

--- js/encomendas.js
+++ js/encomendas.js
@@ -3,13 +3,41 @@ for(i=0; i < encomenda_cliente.length; i++) {
      let clientes = encomenda_cliente[i];
      console.log(clientes)
      // pega cada elemento filho do td
-     let qtd = clientes.children[2]. textContent 
-     let unitario = clientes.children[3].textContent 
-     console.log(qtd)
-     console.log(unitario)
-     let num_qtd = parseInt(qtd)
-     let num_unitario = parseFloat(unitario.replace(',', '.'))
-     let resultado = num_qtd * num_unitario; // armazena cada resultado
-     console.log(resultado)
-     clientes.children[4].textContent = resultado.toFixed(2)
+     // quantidade
+     let tab_qtd = clientes.children[2].textContent 
+     // valor unitário
+     let tab_unitario = clientes.children[3].textContent 
+     console.log(tab_qtd)
+     console.log(tab_unitario)
+     
+     if (tab_qtd < 1 || isNaN(tab_qtd)) {
+          // qtd é menor que 1 ou não é numérica
+          clientes.children[2].textContent = "Quantidade inválida!";
+          clientes.children[2].style.backgroundColor = "red";
+          clientes.children[2].style.color = "white"
+     } else {
+          clientes.children[4].textContent = calculaTotal(tab_qtd, tab_unitario)
+     } if (tab_unitario <= 0 || isNaN(tab_unitario)) {
+           // qtd é menor que 1 ou não é numérica
+           clientes.children[3].textContent = "Valor unitário inválido!";
+           clientes.children[3].style.backgroundColor = "white";
+           clientes.children[3].style.color = "red";
+     } else {
+          clientes.children[4].textContent = calculaTotal(tab_qtd,tab_unitario)
+          clientes.children[3].textContent = formataValor(parseFloat(tab_unitario))
+
+     } 
+      
+     }
+   
+
+function calculaTotal(qtd, unitario) {
+     let total = 0
+     total = qtd * unitario
+     return formataValor(total)
+}
+// converte os valores para (R$)
+function formataValor(valor) {
+     let valor_formatado = valor.toLocaleString('pt-BR' , {style: 'currency' , currency: 'BRL'});
+     return valor_formatado
 }
